38105655a2726616c5319e363f0f29e63c4aed54
Mahmoud Hawara <154727577+Mahmoud-Hawara@users.noreply.github.com>
2024-01-21 16:34:35 +0200

    adding the navbar for visitor user

---
+++ Frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App h-screen bg-main_bg_color_dark">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
