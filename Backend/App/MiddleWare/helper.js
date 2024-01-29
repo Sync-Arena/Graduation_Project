fccebbbc3b21b8bb2385dd54b25c433c988e383b
Ibrahim-Kaldesh <ebrahim195011@feng.bu.edu.eg>
2024-01-29 16:59:42 +0200

    Sending JWT via Cookie

--- Backend/App/MiddleWare/helper.js
+++ Backend/App/MiddleWare/helper.js
@@ -9,9 +9,3 @@ export const resGen = function (res, statusCode, apiStatus, message, data) {
     data,
   });
 };
-
-export const checkUserID = cathcAsync(async function (req, res, next) {
-  const user = await userModel.findById(req.params.id);
-  if (!user) return next(new AppError("User not found !!", 404));
-  next();
-});
