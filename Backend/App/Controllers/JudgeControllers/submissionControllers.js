0a266d2ffd6871f487b5a77e09d2cc1383f6e076
Khaled <Khaled195341@feng.bu.edu.eg>
2024-02-08 00:47:28 +0200

    submit problem

--- Backend/App/Controllers/JudgeControllers/submissionControllers.js
+++ Backend/App/Controllers/JudgeControllers/submissionControllers.js
@@ -1,19 +1,64 @@
 import submissionModel from "../../../Database/Models/JudgeModels/submissionModel.js";
 import { cathcAsync } from "../errorControllers/errorContollers.js";
+import getURL from "../../../util/apiPolygon.js"
+import axios from "axios"
+import { compile } from "./compilerController.js"
+import { stdWcmpCpp } from "../../../util/stdCheckers.js"
 
-export const createSubmission = cathcAsync(async (req, res, next) => {
-  // Get IDs from request if exists
-  if (!req.user) req.body.user = req.user.id;
-  if (!req.body.contest) req.body.contest = req.params.contestId;
-  if (!req.body.problem) req.body.problem = req.params.problemId;
+// {problemId: param, code, compilerCode, }
+export const submit = cathcAsync(async (req, res, next) => {
+	// Get IDs from request if exists
+	const { compiler, code, problemId } = req.body
 
-  const submission = await submissionModel.create(req.body);
+	const problemInfo = await axios.get(getURL("problem.info", { problemId }))
+	const { timeLimit, memoryLimit } = problemInfo.data.result
 
-  res.status(201).json({
-    message: "Submission created successfully",
-    submission,
-  });
-});
+	const problemTests = await axios.get(
+		getURL("problem.tests", { problemId, testset: "tests" })
+	)
+	const testsIndexs = []
+	for (const obj of problemTests.data.result) {
+		testsIndexs.push(obj.index)
+	}
+
+	for (let i = 0; i < 1 /*testsIndexs.length*/; i++) {
+		const input = await axios.get(
+			getURL("problem.testInput", {
+				problemId,
+				testset: "tests",
+				testIndex: testsIndexs[i],
+			})
+		)
+		const answer = await axios.get(
+			getURL("problem.testAnswer", {
+				problemId,
+				testset: "tests",
+				testIndex: testsIndexs[i],
+			})
+		)
+		try {
+			const response = await compile({
+				id: compiler,
+				code,
+				input: input.data,
+				answer: answer.data,
+				time_limit: timeLimit,
+				memory_limit: memoryLimit,
+				checker: stdWcmpCpp, // need to specify right one
+			})
+			return res.status(200).json(response)
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	// const submission = await submissionModel.create(req.body)
+
+	res.status(201).json({
+		message: "Submission created successfully",
+		submission,
+	})
+})
 
 export const mySubmissions = cathcAsync(async function (req, res, next) {
   // Note: populate is called in query Middleware
