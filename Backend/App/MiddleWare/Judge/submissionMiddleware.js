ed63b38103322b980d225d1246c5b94da65a0f4c
Ibrahim-Kaldesh <ebrahim195011@feng.bu.edu.eg>
2024-02-16 16:02:48 +0200

    (sumbit) function has been transferred to submisssionMiddleware.js

--- Backend/App/MiddleWare/Judge/submissionMiddleware.js
+++ Backend/App/MiddleWare/Judge/submissionMiddleware.js
@@ -1,8 +1,93 @@
 import { cathcAsync } from "../../Controllers/errorControllers/errorContollers.js";
+import getURL from "../../../util/apiPolygon.js";
+import axios from "axios";
+import { compile } from "../../../App/Controllers/JudgeControllers/compilerController.js";
+import { stdWcmpCpp } from "../../../util/stdCheckers.js";
 
-export const checkProblemStatus = cathcAsync(async function (req, res, next) {
-  req.body.targetMinute = "40";
-  req.body.verdict = "Accepted";
-  console.log(req.body);
+// {problemId: param, code, compilerCode, }
+export const submit = cathcAsync(async (req, res, next) => {
+  // Get IDs from request if exists
+  const { compiler, code, problemId } = req.body;
+
+  const problemInfo = await axios.get(getURL("problem.info", { problemId }));
+  const { timeLimit, memoryLimit } = problemInfo.data.result;
+
+  const problemTests = await axios.get(
+    getURL("problem.tests", { problemId, testset: "tests" })
+  );
+  const testsIndexs = [];
+  for (const obj of problemTests.data.result) {
+    testsIndexs.push(obj.index);
+  }
+
+  const status = [],
+    answers = [],
+    stdin = [],
+    stdout = [];
+
+  let languageName,
+    memory = -1,
+    time = -1,
+    wholeStatus = "Accepted";
+
+  for (let i = 0; i < 1 /*testsIndexs.length*/; i++) {
+    const input = await axios.get(
+      getURL("problem.testInput", {
+        problemId,
+        testset: "tests",
+        testIndex: testsIndexs[i],
+      })
+    );
+    const answer = await axios.get(
+      getURL("problem.testAnswer", {
+        problemId,
+        testset: "tests",
+        testIndex: testsIndexs[i],
+      })
+    );
+    const response = await compile({
+      id: compiler,
+      code,
+      input: input.data,
+      answer: `${answer.data}`,
+      time_limit: timeLimit,
+      memory_limit: memoryLimit,
+      checker: stdWcmpCpp, // need to specify right one
+    });
+
+    const st = {
+      description: response.status.description,
+      pr: response.status.pr,
+    };
+
+    stdin.push(response.stdin);
+    stdout.push(response.stdout);
+    answers.push(`${answer.data}`);
+    status.push(st);
+
+    languageName = response.language.name;
+    memory = Math.max(memory, response.memory);
+    time = Math.max(time, +response.time);
+
+    if (response.status.description !== "Accepted") {
+      wholeStatus = "Not Accepted";
+      break;
+    }
+  }
+
+  req.submissionModel = {
+    sourceCode: code,
+    languageName,
+    problemId,
+    stdin,
+    stdout,
+    answers,
+    status,
+    memory,
+    wholeStatus,
+    time: `${time}`,
+    user: req.user._id,
+  };
+
   next();
 });
