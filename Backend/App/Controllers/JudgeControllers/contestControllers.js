4066afea431322224e8dfcb13fb395dac634eb2d
Khaled <Khaled195341@feng.bu.edu.eg>
2024-03-20 12:02:05 +0200

    abosalem merge fix

--- Backend/App/Controllers/JudgeControllers/contestControllers.js
+++ Backend/App/Controllers/JudgeControllers/contestControllers.js
@@ -131,6 +131,72 @@ export const deleteProblem = cathcAsync(async (req, res, next) => {
 	)
 })
 
+export const AllSubmissionsOfContest = cathcAsync(async (req, res, next) => {
+	const { contestId } = req.body
+
+	if (!contestId) next(new AppError("Contest Id missing", 400))
+
+	const submissions = await submissionModel.find({ contest: contestId })
+	resGen(res, 200, "success", "All submissions of the contest", submissions)
+})
+
+export const UserSubmissionsInContest = cathcAsync(async (req, res, next) => {
+	const userId = req.user._id
+	const { contestId } = req.body
+
+	if (!contestId) next(new AppError("Contest Id missing", 400))
+
+	const submissions = await submissionModel.find({
+		user: userId,
+		contest: contestId,
+	})
+	resGen(res, 200, "success", "Your submissions in the contest", submissions)
+})
+
+// Controller function to register user for a contest
+export const registerForContest = cathcAsync(async (req, res, next) => {
+	try {
+		const userId = req.user._id
+		const { contestId } = req.body
+
+		const contest = await Contest.findById(contestId)
+
+		if (!contest) next(new AppError("Contest not found", 400))
+
+		const updated = await Contest.findByIdAndUpdate(
+			contestId,
+			{ $push: { paticipatedUsers: userId } },
+			{ new: true }
+		)
+
+		resGen(res, 200, "success", "User registered for the contest")
+	} catch (error) {
+		throw new AppError(`Error registering user for contest:${error}`)
+	}
+})
+
+// Controller function to cancel user registration for a contest
+export const cancelContestRegistration = cathcAsync(async (req, res, next) => {
+	try {
+		const userId = req.user._id
+		const { contestId } = req.body
+
+		const contest = await Contest.findById(contestId)
+
+		if (!contest) next(new AppError("Contest not found", 400))
+
+		const updated = await Contest.findByIdAndUpdate(
+			contestId,
+			{ $pull: { paticipatedUsers: userId } },
+			{ new: true }
+		)
+
+		resGen(res, 200, "success", "User registration canceled for the contest")
+	} catch (error) {
+		throw new AppError(`Error canceling user registration for contest:${error}`)
+	}
+})
+
 export const addAdminToContest = asyncHandler(async (req, res, next) => {
 	const { userId, contestId } = req.body
 	if (!userId) next(new AppError("Please provide a user ID", 404))
