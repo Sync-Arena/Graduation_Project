d11ffd14493b36f21dceee0ae47373294b8ca3a8
Khaled <Khaled195341@feng.bu.edu.eg>
2024-06-05 17:22:16 +0300

    problem set

--- Backend/Routes/JudgeRoutes/problemRoutes.js
+++ Backend/Routes/JudgeRoutes/problemRoutes.js
@@ -2,12 +2,13 @@ import express from "express"
 import {
 	createProblem,
 	getMyProblems,
+	getallproblems
 } from "../../App/Controllers/JudgeControllers/problemController.js"
 
 const router = express.Router()
 
 router.route("/problemCreate-update").post(createProblem)
 router.route("/myproblems").get(getMyProblems)
-
+router.route("/problemset").get(getallproblems)
 
 export default router
