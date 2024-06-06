da1a0fbf990273b19a31605c9e2def30d33d096f
modessam <mahmoud195953@feng.bu.edu.eg>
2024-06-06 14:07:50 +0300

    --

--- Backend/App/Controllers/JudgeControllers/submissionControllers.js
+++ Backend/App/Controllers/JudgeControllers/submissionControllers.js
@@ -3,17 +3,18 @@ import { cathcAsync } from '../errorControllers/errorContollers.js'
 
 export const createSubmission = cathcAsync(async function (req, res, next) {
     // console.log(req.submissionModel)
+    let submission
     req.members.forEach(async (element) => {
         let obj = req.submissionModel
         obj.teamId = req.teamId
         obj.members = req.members
-        const submission = await submissionModel.create(obj)
+        submission = await submissionModel.create(obj)
     })
     /// update the number of users to solve the problem in problem schema ===> to be done
     // check if the problem is solved before from user-contest relation
     res.status(201).json({
         message: 'Submission created successfully',
-        // submission,
+        submission,
     })
 })
 
