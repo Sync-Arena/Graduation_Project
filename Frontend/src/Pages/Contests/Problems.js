c771d0c88c8a5f39a0e6c74b06dbb04fe42d0701
AhmedHamdy3 <ahmed195053@feng.bu.edu.eg>
2024-06-23 03:06:10 +0300

    Loading

--- Frontend/src/Pages/Contests/Problems.js
+++ Frontend/src/Pages/Contests/Problems.js
@@ -11,6 +11,7 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import AuthContext from "../../Context/AuthProvider";
+import Loading from "../Loading/Loading"; // Assuming correct path to your Loading component
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -35,21 +36,20 @@ function convertToAlphabetic(index) {
 }
 
 function Problems() {
+  const [loading, setLoading] = useState(false)
   const InContest = useRef(0);
-
   const pageSize = 20;
   const [currentPage, setCurrentPage] = useState(1);
   const navigate = useNavigate();
-
   const [problemsArray, setProblemsArray] = useState([]);
   const totalProblems = problemsArray.length;
-
   const startIndex = (currentPage - 1) * pageSize;
   const endIndex = startIndex + pageSize;
   const { auth } = useContext(AuthContext);
   const contestId = useParams();
   // console.log(contestId)
   useEffect(() => {
+    setLoading(true)
     let fetchData = async () => {
       try {
         const config = {
@@ -64,6 +64,7 @@ function Problems() {
       } catch (err) {
         console.error(err);
       } finally {
+        setLoading(false)
       }
     };
     fetchData();
@@ -90,115 +91,119 @@ function Problems() {
   return (
     <div className="overflow-x-auto mt-10 flex">
       <div className="w-full">
-        <table className="w-full text-left rtl:text-right text-second_font_color_dark">
-          <colgroup>
-            <col style={{ width: "10%" }} />
-            <col style={{ width: "50%" }} />
-            <col style={{ width: "20%" }} />
-            <col style={{ width: "10%" }} />
-            <col style={{ width: "10%" }} />
-          </colgroup>
-          <thead className="text-third_font_color_dark">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                #
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Name
-              </th>
-              <th scope="col" className="px-6 py-3"></th>
-              <th scope="col" className="px-6 py-3"></th>
-              <th scope="col" className="px-6 py-3"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {problemsArray.map((problem, index) => (
-              <tr
-                key={index}
-                className={`${
-                  index % 2 === 0 ? "bg-second_bg_color_dark" : ""
-                }`}
-                onClick={() => handleProblemClick(problem._id)}
-                style={{ cursor: "pointer" }}
-              >
-                <td className="px-6 py-4">{convertToAlphabetic(index)}</td>
-                <td className="px-6 py-4 font-semibold">{problem.name}</td>
-                <td className="px-6 py-4">
-					{problem.timeLimit}ms, {problem.memoryLimit}MB
-				</td>
-                <td className="px-6 py-4 text-center">
-                  {problem.state === "A" ||
-                  problem.state === "F" ||
-                  problem.state === "FSP" ? (
-                    <FaCheck style={{ color: "green", fontSize: "1.3rem" }} />
-                  ) : problem.state === "P" ? (
-                    <FaSpinner
-                      className="animate-spin"
-                      style={{ color: "orange", fontSize: "1.3rem" }}
-                    />
-                  ) : (
-                    problem.state === "W" && (
-                      <IoClose style={{ color: "red", fontSize: "1.3rem" }} />
-                    )
-                  )}
-                </td>
-                <td className="px-6 py-4">
-                  <div className="flex items-center">
-                    {InContest.current ? (
-                      problem.state === "A" ? (
-                        <TbBalloonFilled
-                          style={{ fontSize: "1.5rem", color: problem.color }}
-                        />
-                      ) : problem.state === "F" ? (
-                        <GiBalloons
-                          style={{ fontSize: "1.5rem", color: problem.color }}
-                        />
-                      ) : problem.state === "FSP" ? (
-                        <BsFillBalloonHeartFill
-                          style={{ fontSize: "1.5rem", color: problem.color }}
-                        />
-                      ) : null
-                    ) : (
-                      <>
-                        <LuUser2 style={{ fontSize: "1.2rem" }} />
-                        <span className="block ml-4">
-                          {problem.numberOfSolvers}
-                        </span>
-                      </>
-                    )}
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        {totalProblems > 20 && (
-          <div className="flex justify-end my-6 items-center">
-            <FaAngleLeft
-              className="text-main_font_color_dark cursor-pointer mr-2"
-              onClick={() => handlePageChange(Math.max(currentPage - 1, 1))}
-            />
-            {visiblePages.map((page) => (
-              <button
-                key={page}
-                onClick={() => handlePageChange(page)}
-                className={`rounded-full mx-1 text-main_font_color_dark ${
-                  currentPage === page ? "bg-main_heighlight_color_dark " : ""
-                } ${
-                  String(page).length === 1 ? "px-3 py-1" : "px-2 py-1"
-                } cursor-pointer`}
-              >
-                {page}
-              </button>
-            ))}
-            <FaAngleRight
-              className="text-main_font_color_dark cursor-pointer ml-2"
-              onClick={() =>
-                handlePageChange(Math.min(currentPage + 1, totalPages))
-              }
-            />
-          </div>
-        )}
+        {
+          loading ?
+            <div className="mt-32 flex justify-center"><Loading /></div>
+            :
+            <div>
+              <table className="w-full text-left rtl:text-right text-second_font_color_dark">
+                <colgroup>
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "50%" }} />
+                  <col style={{ width: "20%" }} />
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "10%" }} />
+                </colgroup>
+                <thead className="text-third_font_color_dark">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      #
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Name
+                    </th>
+                    <th scope="col" className="px-6 py-3"></th>
+                    <th scope="col" className="px-6 py-3"></th>
+                    <th scope="col" className="px-6 py-3"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {problemsArray.map((problem, index) => (
+                    <tr
+                      key={index}
+                      className={`${index % 2 === 0 ? "bg-second_bg_color_dark" : ""
+                        }`}
+                      onClick={() => handleProblemClick(problem._id)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      <td className="px-6 py-4">{convertToAlphabetic(index)}</td>
+                      <td className="px-6 py-4 font-semibold">{problem.name}</td>
+                      <td className="px-6 py-4">
+                        {problem.timeLimit}ms, {problem.memoryLimit}MB
+                      </td>
+                      <td className="px-6 py-4 text-center">
+                        {problem.state === "A" ||
+                          problem.state === "F" ||
+                          problem.state === "FSP" ? (
+                          <FaCheck style={{ color: "green", fontSize: "1.3rem" }} />
+                        ) : problem.state === "P" ? (
+                          <FaSpinner
+                            className="animate-spin"
+                            style={{ color: "orange", fontSize: "1.3rem" }}
+                          />
+                        ) : (
+                          problem.state === "W" && (
+                            <IoClose style={{ color: "red", fontSize: "1.3rem" }} />
+                          )
+                        )}
+                      </td>
+                      <td className="px-6 py-4">
+                        <div className="flex items-center">
+                          {InContest.current ? (
+                            problem.state === "A" ? (
+                              <TbBalloonFilled
+                                style={{ fontSize: "1.5rem", color: problem.color }}
+                              />
+                            ) : problem.state === "F" ? (
+                              <GiBalloons
+                                style={{ fontSize: "1.5rem", color: problem.color }}
+                              />
+                            ) : problem.state === "FSP" ? (
+                              <BsFillBalloonHeartFill
+                                style={{ fontSize: "1.5rem", color: problem.color }}
+                              />
+                            ) : null
+                          ) : (
+                            <>
+                              <LuUser2 style={{ fontSize: "1.2rem" }} />
+                              <span className="block ml-4">
+                                {problem.numberOfSolvers}
+                              </span>
+                            </>
+                          )}
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              {totalProblems > 20 && (
+                <div className="flex justify-end my-6 items-center">
+                  <FaAngleLeft
+                    className="text-main_font_color_dark cursor-pointer mr-2"
+                    onClick={() => handlePageChange(Math.max(currentPage - 1, 1))}
+                  />
+                  {visiblePages.map((page) => (
+                    <button
+                      key={page}
+                      onClick={() => handlePageChange(page)}
+                      className={`rounded-full mx-1 text-main_font_color_dark ${currentPage === page ? "bg-main_heighlight_color_dark " : ""
+                        } ${String(page).length === 1 ? "px-3 py-1" : "px-2 py-1"
+                        } cursor-pointer`}
+                    >
+                      {page}
+                    </button>
+                  ))}
+                  <FaAngleRight
+                    className="text-main_font_color_dark cursor-pointer ml-2"
+                    onClick={() =>
+                      handlePageChange(Math.min(currentPage + 1, totalPages))
+                    }
+                  />
+                </div>
+              )}
+            </div>
+        }
       </div>
     </div>
   );
--- Frontend/src/Pages/Profile/ProfileRightSide/ProfileOverview/ProfileOverview.js
+++ Frontend/src/Pages/Profile/ProfileRightSide/ProfileOverview/ProfileOverview.js
@@ -1,72 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CalenderHeatMap from "./CalenderHeatMap";
 import RatingProgress from "./RatingProgress";
+import Loading from "../../../Loading/Loading"; // Assuming correct path to your Loading component
 
 const ProfileOverview = () => {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    console.log("das")
+    setTimeout(() => { setLoading(false) }, 2000)
+  }, [loading])
   return (
     <div className="my-6">
-      <div className="bg-second_bg_color_dark rounded-md p-8">
-        <h2 className="font-semibold text-lg mb-8 text-center text-second_font_color_dark">
-          Rating Progress Over Time
-        </h2>
-        <RatingProgress />
-      </div>
-      <div className="mt-6 bg-second_bg_color_dark rounded-md p-8">
-        <h2 className="font-semibold text-lg mb-8 text-center text-fourth_font_color_dark">
-          Rating Progress Over Time
-        </h2>
-        <CalenderHeatMap />
-      </div>
-      <div className="flex justify-between mt-6">
-        <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
-          <p className="font-semibold text-lg text-third_font_color_dark">
-            100 Problems
-          </p>
-          <p className="text-sm text-fourth_font_color_dark">
-            solved for all time
-          </p>
+      {loading ?
+        <div className="mt-32 flex justify-center"><Loading /></div>
+
+        :
+        <div>
+          <div className="bg-second_bg_color_dark rounded-md p-8">
+            <h2 className="font-semibold text-lg mb-8 text-center text-second_font_color_dark">
+              Rating Progress Over Time
+            </h2>
+            <RatingProgress />
+          </div>
+          <div className="mt-6 bg-second_bg_color_dark rounded-md p-8">
+            <h2 className="font-semibold text-lg mb-8 text-center text-fourth_font_color_dark">
+              Rating Progress Over Time
+            </h2>
+            <CalenderHeatMap />
+          </div>
+          <div className="flex justify-between mt-6">
+            <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
+              <p className="font-semibold text-lg text-third_font_color_dark">
+                100 Problems
+              </p>
+              <p className="text-sm text-fourth_font_color_dark">
+                solved for all time
+              </p>
+            </div>
+            <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
+              <p className="font-semibold text-lg text-third_font_color_dark">
+                50 Problems
+              </p>
+              <p className="text-sm text-fourth_font_color_dark">
+                solved for the last year
+              </p>
+            </div>
+            <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
+              <p className="font-semibold text-lg text-third_font_color_dark">
+                25 Problems
+              </p>
+              <p className="text-sm text-fourth_font_color_dark">
+                solved for the last month
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-between mt-6">
+            <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
+              <p className="font-semibold text-lg text-third_font_color_dark">
+                20 Days
+              </p>
+              <p className="text-sm text-fourth_font_color_dark">in a row max</p>
+            </div>
+            <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
+              <p className="font-semibold text-lg text-third_font_color_dark">
+                1 Day
+              </p>
+              <p className="text-sm text-fourth_font_color_dark">
+                in a row for the last year
+              </p>
+            </div>
+            <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
+              <p className="font-semibold text-lg text-third_font_color_dark">
+                0 Days
+              </p>
+              <p className="text-sm text-fourth_font_color_dark">
+                in a row for the last month
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
-          <p className="font-semibold text-lg text-third_font_color_dark">
-            50 Problems
-          </p>
-          <p className="text-sm text-fourth_font_color_dark">
-            solved for the last year
-          </p>
-        </div>
-        <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
-          <p className="font-semibold text-lg text-third_font_color_dark">
-            25 Problems
-          </p>
-          <p className="text-sm text-fourth_font_color_dark">
-            solved for the last month
-          </p>
-        </div>
-      </div>
-      <div className="flex justify-between mt-6">
-        <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
-          <p className="font-semibold text-lg text-third_font_color_dark">
-            20 Days
-          </p>
-          <p className="text-sm text-fourth_font_color_dark">in a row max</p>
-        </div>
-        <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
-          <p className="font-semibold text-lg text-third_font_color_dark">
-            1 Day
-          </p>
-          <p className="text-sm text-fourth_font_color_dark">
-            in a row for the last year
-          </p>
-        </div>
-        <div className="flex flex-col items-center rounded-md py-8 bg-second_bg_color_dark max-w-[280px] w-[280px]">
-          <p className="font-semibold text-lg text-third_font_color_dark">
-            0 Days
-          </p>
-          <p className="text-sm text-fourth_font_color_dark">
-            in a row for the last month
-          </p>
-        </div>
-      </div>
+      }
     </div>
   );
 };
